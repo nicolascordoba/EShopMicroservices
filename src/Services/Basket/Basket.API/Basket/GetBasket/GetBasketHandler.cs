fc5e1de9685eab15d314c45536fba49b62a0171a
nacordoba <nicolas_cba15@hotmail.com>
2025-09-04 12:08:52 -0300

    initiate getbasket logic

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,11 +1,8 @@
-﻿using Basket.API.Models;
-using BuildingBlocks.CQRS;
-
-namespace Basket.API.Basket.GetBasket
+﻿namespace Basket.API.Basket.GetBasket
 {
     public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
     public record GetBasketResult(ShoppingCart Cart);
-    public class GetBasketHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
         public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
         {
