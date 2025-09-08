5dc88932e1de904badc1fea7d064888e7d21b58d
nacordoba <nicolas_cba15@hotmail.com>
2025-09-08 11:05:39 -0300

    create a cache decorated class

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -7,7 +7,7 @@
     {
         public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
         {
-            var basket = await repository.GetBasket(query.UserName);
+            var basket = await repository.GetBasket(query.UserName, cancellationToken);
 
             return new GetBasketResult(basket);
         }
