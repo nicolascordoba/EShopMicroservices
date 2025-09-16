c3618ab943bb7bcf3469d37e880e85e28807ee6d
nacordoba <nicolas_cba15@hotmail.com>
2025-09-16 09:48:18 -0300

    Add models, valueObjects, and internal validations for each entity Include all methods to create, update and remove an Order Aggregate

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,72 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Order : Aggregate<OrderId>
+    {
+        private readonly List<OrderItem> _orderItems = new();
+
+        public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+        public CustomerId Customer { get; private set; } = default!;
+        public OrderName OrderName { get; private set; } = default!;
+        public Address ShippingAddress { get; private set; } = default!;
+        public Address BillingAddress { get; private set; } = default!;
+        public Payment Payment { get; private set; } = default!;
+        public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+        public decimal TotalPrice
+        {
+            get => OrderItems.Sum(x => x.Price * x.Quantity);
+            private set { }
+        }
+
+        public static Order Create(OrderId id, CustomerId customer, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+        {
+            var order = new Order
+            {
+                Id = id,
+                Customer = customer,
+                OrderName = orderName,
+                ShippingAddress = shippingAddress,
+                BillingAddress = billingAddress,
+                Status = OrderStatus.Pending
+            };
+
+            order.AddDomainEvent(new OrderCreatedEvent(order));
+
+            return order;
+        }
+
+        public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus status)
+        {
+            OrderName = orderName;
+            ShippingAddress = shippingAddress;
+            BillingAddress = billingAddress;
+            Payment = payment;
+            Status = status;
+
+            //this --> hace referencia a la entidad Order actual
+            AddDomainEvent(new OrderUpdatedEvent(this));
+        }
+        //Add new item to Order Aggregate
+        public void Add(ProductId productId, int quantity, decimal price)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            //call to internal constructor only accesible in the Order domain
+            var orderItem = new OrderItem(Id, productId, quantity, price);
+
+            //Add order Item into the current OrderAggregate
+            _orderItems.Add(orderItem);
+        }
+        //Remove an item from Order Aggregate
+        public void Remove(ProductId productId)
+        {
+            var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+
+            if (orderItem is not null)
+            {
+                _orderItems.Remove(orderItem);
+            }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class OrderItem : Entity<OrderItemId>
+    {
+        //OrderId and ProductId are strongly type ID
+        //Defined to avoid wrong order when pass information parameters to the constructor
+        //constructor is internal due to the creation of the OrderItems depends on the Aggregation Order who manage the status of the Order and OrderItems
+        //OrderItem should be created only by agregate of the order (IMPORTANT)
+        internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+        {
+            Id = OrderItemId.Of(Guid.NewGuid());
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+            Price = price;
+        }
+        public OrderId OrderId { get; private set; } = default!;
+        public ProductId ProductId { get; private set; } = default!;
+        public int Quantity { get; private set; } = default!;
+        public decimal Price { get; private set; } = default!;
+    }
+}
